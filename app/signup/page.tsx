84e8b2db64d1e71bdca5f231a34025b1162c6668
apenukonda <aryanpenukonda@gmail.com>
2026-01-07 18:36:54 +0530

    build issue - vercel

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react"
 import SignupClient from "./SignupClient"
 
+export const dynamic = "force-dynamic"
+
 export default function SignupPage() {
   return (
     <Suspense
